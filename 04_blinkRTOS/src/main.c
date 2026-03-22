db7af311ee2cd4a553af50fd0f13c3c6c3187f0b
Bryce Klippenstein <bryce.klippenstein@gmail.com>
2026-03-22 02:59:26 -0600

    Working single blinky

--- 04_blinkRTOS/src/main.c
+++ 04_blinkRTOS/src/main.c
@@ -6,56 +6,46 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
-#include "blinky.h"
+// #include "blinky.h"
 
-#define STACK_SIZE 1024
-#define PRIORITY 5
-#define NUM_THREADS 2
-
-K_THREAD_STACK_ARRAY_DEFINE(thread_stacks, NUM_THREADS, STACK_SIZE)
-struct k_thread thread_data[2];
+// K_THREAD_STACK_ARRAY_DEFINE(thread_stacks, NUM_THREADS, STACK_SIZE);
+// struct k_thread thread_data[2];
 
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS0   1000
-#define SLEEP_TIME_MS1   900
 
 /* The devicetree node identifier for the "training_led" alias. */
-#define LED0_NODE DT_ALIAS(led_a)
-#define LED1_NODE DT_ALIAS(led_b)
+#define LED0_NODE DT_ALIAS(training_led)
 
-static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
-static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
+static void my_thread_handler(void *dummy1, void *dummy2, void *dummy3) {
+	int ret;
+
+	if (!device_is_ready(led.port)) {
+		return;
+	}
+
+	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		return;
+	}
+
+	while (1) {
+		gpio_pin_toggle_dt(&led);
+		k_sleep(K_SECONDS(1));
+	}
+}
+
+K_THREAD_DEFINE(my_thread, 1024,
+				my_thread_handler, NULL, NULL, NULL,
+				K_LOWEST_APPLICATION_THREAD_PRIO, 0, 0);
 
 /* Main function */
 int main(void)
 {
 	while (1)
 	{
-		k_thread_create(
-				&thread_data[0],
-				thread_stacks[0],
-				STACK_SIZE,
-				blinky,
-				(void *)&led0,
-				(void *)1000,
-				NULL,
-				K_LOWEST_APPLICATION_THREAD_PRIO,
-				0,
-				K_NO_WAIT);
-
-		k_thread_create(
-				&thread_data[1],
-				thread_stacks[1],
-				STACK_SIZE,
-				blinky,
-				(void *)&led1,
-				(void *)950,
-				NULL,
-				K_LOWEST_APPLICATION_THREAD_PRIO,
-				0,
-				K_NO_WAIT);
-
-		k_msleep(10000);
-		// k_yield();
+		k_msleep(1000);
 	}
 }
